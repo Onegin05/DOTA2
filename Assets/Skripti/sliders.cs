28f739f647df4540464d678d37e7bb7443697cd0
Cebotars <gikon603gmail.com>
2023-05-07 11:59:17 +0300

    Pievienu slaiderus lai mainit telu platumu un garumu

---
+++ Assets/Skripti/sliders.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sliders : MonoBehaviour {
+
+	public GameObject image;
+	public GameObject garums;
+	public GameObject platums;
+
+	public void mainitPlatumu(){
+		float x = platums.GetComponent<Slider> ().value;
+		float a = garums.GetComponent<Slider> ().value;
+		image.transform.localScale = new Vector2 (x * 2f, a);
+
+
+	}
+
+	public void mainitGarumu(){
+		float x = garums.GetComponent<Slider> ().value;
+		float a = platums.GetComponent<Slider> ().value;
+		image.transform.localScale = new Vector2 (a, x*2f);
+
+	}
+
+}
