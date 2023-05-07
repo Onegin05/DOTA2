6962b542d0ed6985b07eb66edd342f9a25d1e0f2
Cebotars <gikon603gmail.com>
2023-05-07 11:38:20 +0300

    Uztaisijy apģerbu ar Toggle un DragAndDrop

---
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;              
+
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{  //intefraces to recognize gestures
+
+	public Canvas canvas;                  
+	RectTransform rectTransform;     
+
+	void Start()
+	{
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+	public void OnPointerDown(PointerEventData e)
+	{
+		Debug.Log("Izdarits kreisais klikskis uz velkama objekta");
+	}
+
+	public void OnBeginDrag(PointerEventData e)
+	{
+		Debug.Log("uzsakta vilksana");
+	}
+	public void OnDrag(PointerEventData e)
+	{
+		Debug.Log("Notiek objekta vilksana!");
+		rectTransform.anchoredPosition += e.delta / canvas.scaleFactor;
+	}
+
+	public void OnEndDrag(PointerEventData e)
+	{
+		Debug.Log("Veikta objekta vilksana...");
+	}
+}
---
+++ Assets/Skripti/toggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggle : MonoBehaviour {
+	public GameObject adidashat;
+	public GameObject guccihat;
+	public GameObject adidasbikses;
+	public GameObject guccibikses;
+	public GameObject adidast;
+	public GameObject guccit;
+	public void showadidashat(bool x){
+		adidashat.SetActive (x);
+
+	}
+	public void showguccihat(bool x){
+		guccihat.SetActive (x);
+	}
+	public void showadidasbikses(bool x){
+		adidasbikses.SetActive (x);
+	}
+	public void showguccibikses(bool x){
+		guccibikses.SetActive (x);
+	}
+
+
+	public void showadidast(bool x){
+		adidast.SetActive (x);
+	}
+	public void showguccit(bool x){
+		guccit.SetActive (x);
+	}
+
+}
