569b42c315b044c844e07f1cf761c9813bf672a2
Cebotars <gikon603gmail.com>
2023-05-07 10:53:25 +0300

    Izveidoju teksta paradišanu ar InputField

---
+++ Assets/Skripti/Teksts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teksts : MonoBehaviour {
+
+	public 	GameObject teksts;
+	public GameObject vecums;
+	public GameObject vards;
+
+	public void paraditTekstu(){
+	
+		teksts.GetComponent<Text>().text=vards.GetComponent<Text>().text+" ir " + vecums.GetComponent<Text>().text + " gadu vecs.";
+
+			
+	}
+
+
+}
